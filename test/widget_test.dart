f3d9d5bbda19188e600cb6407791d21baa1f7267
Khabar app <aymen96gc@gmail.com>
2026-05-08 19:48:57 +0300

    edite extension

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,6 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:khabar/core/routing/app_router.dart';
 import 'package:khabar/khabar_app.dart';
 
 void main() {
