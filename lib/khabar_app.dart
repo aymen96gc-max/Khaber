cac6fde2bf804fd8434b8c64cab2aaa3e42efe97
Khabar app <aymen96gc@gmail.com>
2026-05-08 17:36:32 +0300

    new edit

---
+++ lib/khabar_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class KhabarApp extends StatelessWidget {
+  const KhabarApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:khabar/main.dart';
+import 'package:khabar/khabar_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const KhabarApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
