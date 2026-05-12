f80cca02194b2619070b5cb2a13d39e2906fee1a
Khabar app <aymen96gc@gmail.com>
2026-05-12 15:17:51 +0300

    add firebase option into project

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:khabar/core/routing/app_router.dart';
+import 'package:khabar/firebase_options.dart';
 import 'package:khabar/khabar_app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(KhabarApp(appRouter: AppRouter()));
 }
